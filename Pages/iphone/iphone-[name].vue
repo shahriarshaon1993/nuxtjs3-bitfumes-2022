a1453530244fb94037f6e3d741e15762c5672185
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-10-17 15:05:45 +0600

    Dynamic route

---
+++ Pages/iphone/iphone-[name].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>iPhone {{ name }}</h1>
+    </div>
+</template>
+
+<script setup>
+    const route = useRoute();
+    
+    const name = computed(() => {
+        return route.params.name;
+    });
+</script>
