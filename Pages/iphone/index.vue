e92ab11a6928ad62ba3d502545346a2f270a648c
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-10-17 16:34:56 +0600

    public directory

---
+++ Pages/iphone/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="products mt-5">
+            <div class="priduct">
+                <h1>iPhione 12</h1>
+                <img src="/images/iphn12.jpg" alt="iPhone 12" width="200">
+            </div>
+            <div class="priduct">
+                <h1>iPhione 12 Pro</h1>
+                <img src="/images/iphn12pro.jpg" alt="iPhone 12" width="200">
+            </div>
+        </div>
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <nav class="bg-purple-700 text-white">
+        <nav class="bg-purple-500 shadow-lg p-3 text-white text-lg flex justify-between">
             <NuxtLink to="/">Home</NuxtLink>
             <NuxtLink to="/about">About</NuxtLink>
+            <NuxtLink to="/iphone">iPhones</NuxtLink>
             <NuxtLink to="/profile">Profile</NuxtLink>
         </nav>
 
